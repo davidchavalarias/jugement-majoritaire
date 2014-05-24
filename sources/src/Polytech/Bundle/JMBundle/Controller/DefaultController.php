ce958fa2c0454c1f1bc521201048fd929c0d187d
Rémi Patrizio <artemis.hacktivation@gmail.com>
2014-05-24 21:19:28 +0200

    Ajout de la fonction de login au DefaultController

--- sources/src/Polytech/Bundle/JMBundle/Controller/DefaultController.php
+++ sources/src/Polytech/Bundle/JMBundle/Controller/DefaultController.php
@@ -38,4 +38,24 @@ class DefaultController extends Controller
 
         return $this->render('PolytechJMBundle:Default:stats.html.twig', array('election' => $election));
     }
+
+
+
+        public function loginAction()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+        return $this->render('PolytechJMBundle:Security:login.html.twig', array(
+            // last username entered by the user
+            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+            'error'         => $error,
+        ));
+    }
 }
