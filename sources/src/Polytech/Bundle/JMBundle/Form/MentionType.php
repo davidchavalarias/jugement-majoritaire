7aea39aa3c5addc21984897ec38eae0e476740c7
Rémi Patrizio <artemis.hacktivation@gmail.com>
2014-05-25 12:56:33 +0200

    Ajout des fichiers code.html.twig et MentionType.php

---
+++ sources/src/Polytech/Bundle/JMBundle/Form/MentionType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Polytech\Bundle\JMBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class MentionType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Polytech\Bundle\JMBundle\Entity\Mention'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'polytech_bundle_jmbundle_mention';
+    }
+}
